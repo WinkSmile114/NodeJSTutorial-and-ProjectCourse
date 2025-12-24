15fb6212d5e987b38be01c85341817af8b0bf119
WinkSmile114 <adanbarnett114@gmail.com>
2025-12-23 23:26:40 -0500

    different module setup-function-mind-grenade

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ console.log(data);
 // console.log(names);node
 // console.log(sayHi);
 
+require('./7-mind-grenade');
 
-sayHi('John');
-sayHi(names.saika);
-sayHi(names.michael);
+// sayHi('John');
+// sayHi(names.saika);
+// sayHi(names.michael);
