4fa4269b67c59aa6db597c8b835fbbc43a96d86e
WinkSmile114 <adanbarnett114@gmail.com>
2025-12-28 20:45:07 -0500

    FS module

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const path = require('path');
-console.log(path.sep);
+const { readFileSync, writeFileSync} = require('fs');
 
-const filePath = path.join('/content/', 'subfolder', 'test.txt');
-console.log(filePath);
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
 
-const base = path.basename(filePath, '.txt');
-console.log(base);
-
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
-console.log(absolute);
+writeFileSync(
+    './content/result-sync.txt',
+    `Here is the result: ${first}, ${second}`,
+    {flag: 'a'}
+)
