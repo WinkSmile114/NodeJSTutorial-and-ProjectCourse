82add4e1167b29470ca8409d7a1afa07d1d6e3c0
WinkSmile114 <adanbarnett114@gmail.com>
2025-12-29 23:12:44 -0500

    event loop

---
+++ 1-event-loop-example/2-setTimeout.js
@@ -0,0 +1,7 @@
+// started operation system process
+console.log('first task');
+setTimeout(() => {
+    console.log('second task');
+}, 0);
+console.log('third task');
+// completed and exited operation system process
--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const _ = require('lodash');
+const http = require('http');
 
-const items = [1, [2, [3, 4]]];
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        console.log('home page request event');
+        res.end('Home Page');
+        return;
+    } 
+    if (req.url === '/about') {
+        // Blocking Code !!!!
+        for(let i = 0; i<1000; i++) {
+            for( let j = 0; j<1000; j++) {
+                console.log(`${i} ${j}`);
+            }
+        }
 
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log('hello world');
+        console.log('about page request event');
+        res.end('About Page');
+        return;
+    }
+
+    res.end('Error Page');
+})
+
+server.listen(5000, () => {
+    console.log('Server listening on port: 5000....');
+})
