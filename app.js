f3ac57c0e8dd982573032a0947e1954071d92f98
WinkSmile114 <adanbarnett114@gmail.com>
2026-01-03 00:11:57 -0500

    async pattern - refactoring async

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { readFile } = require('fs')
+const { first } = require('lodash')
 
 const getText = (path) => {
     return new Promise((resolve, reject) => {
@@ -12,7 +13,19 @@ const getText = (path) => {
     })
 }
 
-getText('./content/first.txt')
-    .then((result) => console.log(result))
-    .catch((err) => console.log(err))
+const start = async () => {
+    try {
+        const first = await getText('./content/first.txt')
+        const second = await getText('./content/second.txt')
+        console.log(first, second)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+// getText('./content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))
 
